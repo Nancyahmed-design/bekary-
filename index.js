fb01d909bed2989ac820bfb3fce5af22fd0dfbfe
Nancyahmed-design <nancyahmed2025@gmail.com>
2021-07-26 00:17:32 +0200

    dynamic images

---
+++ index.js
@@ -0,0 +1,26 @@
+function newPicture() {
+
+    document.getElementById("image").src = "./img/images (7).jpeg";
+}
+function oldPicture() {
+    document.getElementById("image").src = "./img/images (14).jpeg";
+
+}
+
+function newPic() {
+
+    document.getElementById("img").src = "./img/images (6).jpg";
+}
+function oldPic() {
+    document.getElementById("img").src = "./img/images (28).jpeg";
+
+}
+
+function newf() {
+
+    document.getElementById("imag").src = "./img/images (30).jpg";
+}
+function old() {
+    document.getElementById("imag").src = "./img/download (12).jpeg";
+
+}
